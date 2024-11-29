c583cc237de41cd8b10a5cc6bfd8d6fe8ced8bc0
salma-h-wagdy <salmahisham139@gmail.com>
2024-11-29 18:19:06 +0200

    removed error el mafrood

--- Drivers/HAL/RelayMod/Relay.c
+++ Drivers/HAL/RelayMod/Relay.c
@@ -1,8 +1,8 @@
 #include <stdbool.h>
-#include "..\Drivers\TivaLib\driverlib\gpio.h"
-//#include "..\Drivers\driverlib\sysctl.h"
-#include "..\Drivers\TivaLib\driverlib\debug.h"
-#include "..\Drivers\TivaLib\inc\hw_memmap.h"
+//#include "..\Drivers\TivaLib\driverlib\gpio.h"
+////#include "..\Drivers\driverlib\sysctl.h"
+//#include "..\Drivers\TivaLib\driverlib\debug.h"
+//#include "..\Drivers\TivaLib\inc\hw_memmap.h"
 
 void Relay_Init (){
   
